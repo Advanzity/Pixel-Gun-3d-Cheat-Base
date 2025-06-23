6f80e2c78a84d5911e5f64e848575c4b3fe76345
Advanzity <63934730+Advanzity@users.noreply.github.com>
2025-06-23 23:21:09 +0000

    dsad

---
+++ Internal DX11 Base/GameHooks/IL2CPP/IL2CPPCore.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "IL2CPPCore.h"
+// This is the ONLY file that should include BNM headers directly
+#include "JQ-BNM-main/BNM-IL2CPP/BNM.hpp"
+
+namespace IL2CPPCore {
+    void Initialize() {
+        IL2CPP::Initialize();
+    }
+}
---
+++ Internal DX11 Base/GameHooks/IL2CPP/IL2CPPCore.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace IL2CPPCore {
+    void Initialize();
+}
